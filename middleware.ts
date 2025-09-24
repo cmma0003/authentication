2bdb4f66ff6affd86adfc7fe3b767dc6ebf12fac
cmorillo <cmorillo@nopan.com>
2025-09-24 08:39:46 +0200

    test for vercel

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from "@/lib/auth/auth";
 
 export const config = {
-    matcher: ['/((?!api/auth|_next/static|_next/image|static|images|.well-known).*)'],
+    matcher: ['/((?!api/auth|signin|_next/static|_next/image|static|images|.well-known).*)'],
 };
