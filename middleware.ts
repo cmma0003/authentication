7adf3e1f64c16b8574b56c8e498e0d6acf34ddec
cmma <cmma87@gmail.com>
2025-09-25 09:14:19 +0200

    test keep matcher, update signIn.tsx

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 export { auth as middleware } from "@/lib/auth/auth";
 
 export const config = {
-    matcher: ['/((?!api/auth|signin|_next/static|_next/image|static|images|.well-known).*)'],
+    //matcher: ['/((?!api/auth|signin|_next/static|_next/image|static|images).*)'],
+    matcher: [
+        '/((?!.+\\.[\\w]+$|_next).*)',
+        '/((?!api).*)'
+    ],
 };
 
 /*
